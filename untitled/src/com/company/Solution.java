e48d0539ccbb0f091f92f38f5440d4a118826092
Oleg Tsymaenko <tsyma@lafox.net>
2020-05-31 16:21:21 +0300

    Oleg's Solution

--- untitled/src/com/company/Solution.java
+++ untitled/src/com/company/Solution.java
@@ -1,14 +1,29 @@
 package com.company;
 
-import java.io.*;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.Files.readAllLines;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.Paths;
+import java.util.List;
 
 /*
 Переопределение сериализации в потоке
 */
 public class Solution implements Serializable, AutoCloseable {
-    private FileOutputStream stream;
+    private transient FileOutputStream stream;
+    private final String fileName;
+    private final static String TEST_DATA ="testStringObject";
 
     public Solution(String fileName) throws FileNotFoundException {
+        this.fileName = fileName;
         this.stream = new FileOutputStream(fileName);
     }
 
@@ -20,12 +35,11 @@ public class Solution implements Serializable, AutoCloseable {
 
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
-        out.close();
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
-        in.close();
+        this.stream = new FileOutputStream(fileName,true);
     }
 
     @Override
@@ -34,7 +48,24 @@ public class Solution implements Serializable, AutoCloseable {
         stream.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String outputFile = "outputFile.txt";
 
+        Solution solution = new Solution(outputFile);
+        solution.writeObject(TEST_DATA);
+
+        File tmpFile = File.createTempFile("tmpFile", ".tmp");
+
+        ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(tmpFile));
+        os.writeObject(solution);
+
+        ObjectInputStream is = new ObjectInputStream(new FileInputStream(tmpFile));
+
+        Solution deserializedSolution = (Solution) is.readObject();
+        deserializedSolution.writeObject(TEST_DATA);
+
+        List<String> strings = readAllLines(Paths.get(outputFile), UTF_8);
+        System.out.println(TEST_DATA.equals(strings.get(0)) ? "OK" : "Файл не содержит данные из п.2");
+        System.out.println(TEST_DATA.equals(strings.get(1)) ? "OK" : "Файл не содержит данные из п.5");
     }
 }
